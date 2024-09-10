658fe102f49aabbdd268813c9e76c422ba9d3021
Rohan Choundiye <113783657+rcc-1995@users.noreply.github.com>
2024-09-10 21:51:05 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js"
-import Razorpay from 'razorpay'
-const stripe = new Razorpay(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe'
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //config variables
 const currency = "inr";
