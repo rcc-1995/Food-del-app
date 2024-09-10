1658cb2b6cddb67bce7444903030b0c5a777dd13
Rohan Choundiye <113783657+rcc-1995@users.noreply.github.com>
2024-09-10 23:48:19 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>Tomato, launched in 2024, is a dynamic food delivery platform designed to simplify the process of ordering food online.It offers users a smooth experience to browse through local restaurants, explore menus, and place orders effortlessly. </p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
@@ -27,8 +27,8 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+91-9322151471</li>
+                <li>tomato.fooddel@gmail.com</li>
             </ul>
         </div>
       </div>
