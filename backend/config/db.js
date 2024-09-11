ce50e1a387e423cc547e86b96fd10afc5db309e6
Rohan Choundiye <113783657+rcc-1995@users.noreply.github.com>
2024-09-11 11:58:16 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect(mongodb+srv://tomatofooddel:Rohan.1995@cluster0.nqcv7.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+   await mongoose.connect('mongodb+srv://tomatofooddel:Rohan.1995@cluster0.nqcv7.mongodb.net/food-del')
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.error("DB Connection Error:", err));
+
     
    
 }
